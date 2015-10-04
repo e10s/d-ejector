0a95e3490ccba2a382efec4b0c9cb0e553f53670
e10s <electrolysis.jp@gmail.com>
2015-10-04 21:46:42 +0900

    move examples

--- ejector_example/source/app.d
+++ ejector_example/source/app.d
@@ -1,5 +1,6 @@
 import ejector;
 
+version(linux)
 void main(){
 	// Assign a device and test the drive.
 	auto e1 = Ejector("/dev/sr0");
@@ -20,6 +21,30 @@ void main(){
 		}
 		else if(status == TrayStatus.CLOSED){
 			e[1].open();
+		}
+	}
+}
+
+
+version(Windows)
+void main(){
+	// Assign a drive letter and test the drive.
+	auto e1 = Ejector("f");
+	e1.ejectable;
+
+	// Try to get an automatically selected drive open.
+	auto e2 = Ejector();
+	e2.open();
+
+	// Search for all ejectable drives and try to toggle open/closed.
+	import std.algorithm, std.ascii;
+	foreach(e; uppercase.map!(a => Ejector(cast(char)a)).filter!(a => a.ejectable)){
+		auto status = e.status;
+		if(status == TrayStatus.OPEN){
+			e.closed();
+		}
+		else if(status == TrayStatus.CLOSED){
+			e.open();
 		}
 	}
 }
