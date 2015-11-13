fc7a2698a488922d9a8ad3e53e134d15acf93865
Kazuya Takahashi <electrolysis.jp@gmail.com>
2015-11-13 23:25:44 +0900

    add FreeBSD example

--- ejector_example/source/app.d
+++ ejector_example/source/app.d
@@ -6,6 +6,36 @@ Distributed under the Boost Software License, Version 1.0.
 
 import ejector;
 
+version(FreeBSD)
+void main()
+{
+    // Assign a device and test the drive.
+    auto e1 = Ejector("/dev/cd0");
+    e1.ejectable;
+
+    // Try to get the default "/dev/cd0" open.
+    auto e2 = Ejector();
+    e2.open();
+
+    // Search for ejectable drives and try to toggle open/closed.
+    import std.algorithm, std.stdio, std.typecons;
+    foreach (e; ["/dev/stdin", "/dev/null", "/dev/dvd", "/no/such/device"]
+        .map!(a => tuple(a, Ejector(a))).filter!(a => a[1].ejectable))
+    {
+        writeln(e[0] ~ " is ejectable!");
+        auto status = e[1].status;
+        if (status == TrayStatus.OPEN)
+        {
+            e[1].closed();
+        }
+        else if (status == TrayStatus.CLOSED)
+        {
+            e[1].open();
+        }
+    }
+}
+
+
 version(linux)
 void main()
 {
@@ -17,7 +47,7 @@ void main()
     auto e2 = Ejector();
     e2.open();
 
-    // Search for all ejectable drives and try to toggle open/closed.
+    // Search for ejectable drives and try to toggle open/closed.
     import std.algorithm, std.stdio, std.typecons;
     foreach (e; ["/dev/stdin", "/dev/null", "/dev/dvd", "/no/such/device"]
         .map!(a => tuple(a, Ejector(a))).filter!(a => a[1].ejectable))
