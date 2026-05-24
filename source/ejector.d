aeb93afd6c0ff0f0aaeba473be02af84e786df48
Kazuya Takahashi <electrolysis.jp@gmail.com>
2026-05-24 22:53:49 +0900

    indev

--- source/ejector.d
+++ source/ejector.d
@@ -6,4 +6,63 @@ Distributed under the Boost Software License, Version 1.0.
 
 module ejector;
 
-public import ejector_base, ejector_win, ejector_freebsd, ejector_linux;
+public import ejector_base;
+import ejector_win;
+
+///
+struct Ejector
+{
+    version (Windows)
+    {
+        private string drive = "";
+        ///
+        this(string driveLetter)
+        {
+            // "a" to "z" or "A" to "Z"
+            import std.uni : isAlpha, toUpper;
+
+            if (driveLetter.length == 1 && driveLetter[0].isAlpha)
+            {
+                drive = driveLetter.toUpper;
+            }
+        }
+        ///
+        this(char driveLetter)
+        {
+            this(cast(string)[driveLetter]);
+        }
+    }
+    else version (linux)
+    {
+        private string drive = "/dev/cdrom";
+    }
+    else version (FreeBSD)
+    {
+        private string drive = "/dev/cd0";
+    }
+    ///
+    @property auto status()
+    {
+        return statusImpl(drive);
+    }
+    ///
+    @property auto ejectable()
+    {
+        return ejectableImpl(drive);
+    }
+    ///
+    @property auto closable()
+    {
+        return closableImpl(drive);
+    }
+    ///
+    auto open()
+    {
+        return openImpl(drive);
+    }
+    ///
+    auto closed()
+    {
+        return closeImpl(drive);
+    }
+}
