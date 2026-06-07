5ad62a32b13e9458e45d6063812f6dde154e4751
Kazuya Takahashi <electrolysis.jp@gmail.com>
2026-06-08 01:57:43 +0900

    implement defaultDrive for linux

--- source/ejector/posix/linux.d
+++ source/ejector/posix/linux.d
@@ -10,14 +10,93 @@ version (linux)
 {
     package mixin template LinuxImpl()
     {
-        // INDEV
+        @property auto defaultDrive()
+        {
+            import std.file : exists;
+            import std.path : buildPath;
+
+            immutable devPath = "/dev";
+            immutable devCdromPath = buildPath(devPath, "cdrom");
+
+            if (devCdromPath.exists)
+            {
+                return devCdromPath;
+            }
+
+            immutable cdDrivePrefix = "sr";
+
+            import std.concurrency : Generator;
+
+            auto r = new Generator!size_t({
+                import std.file : FileException;
+
+                immutable sysClassBlockPath = "/sys/class/block";
+
+                try
+                {
+                    import std.file : dirEntries, SpanMode;
+
+                    foreach (entry; dirEntries(sysClassBlockPath, SpanMode.shallow))
+                    {
+                        try
+                        {
+                            import std.algorithm : startsWith;
+                            import std.path : baseName;
+
+                            if (entry.isDir && entry.baseName.startsWith(cdDrivePrefix))
+                            {
+                                immutable k = entry.baseName[cdDrivePrefix.length .. $];
+
+                                import std.algorithm : all;
+                                import std.ascii : isDigit;
+                                import std.utf : byChar;
+
+                                if (k != "" && k.byChar.all!isDigit)
+                                {
+                                    import std.concurrency : yield;
+                                    import std.conv : to;
+
+                                    yield(k.to!size_t);
+                                }
+                            }
+                        }
+                        catch (FileException fe)
+                        {
+                            continue;
+                        }
+                    }
+                }
+                catch (FileException fe)
+                {
+                }
+            });
+
+            if (r.empty)
+            {
+                return "";
+            }
+
+            import std.algorithm : minElement;
+            import std.conv : to;
+
+            return buildPath(devPath, cdDrivePrefix ~ r.minElement.to!string);
+        }
+
         auto getTargetDrive(string drivePathName)
         {
             if (drivePathName == "")
             {
-                immutable defaultDrive = "/dev/cdrom";
-                logGeneric("Target drive: <" ~ defaultDrive ~ ">");
-                return GetTargetDriveResult(true, defaultDrive);
+                immutable defaultDrive_ = defaultDrive;
+                if (defaultDrive_ == "")
+                {
+                    logGeneric("No optical drive /dev/cdrom or /dev/sr* found");
+                    return GetTargetDriveResult(false, "");
+                }
+                else
+                {
+                    logGeneric("Target drive: <" ~ defaultDrive_ ~ ">");
+                    return GetTargetDriveResult(true, defaultDrive_);
+                }
             }
 
             logGeneric("Target drive: <" ~ drivePathName ~ ">");
@@ -59,21 +138,20 @@ version (linux)
                 dxferp: &response,
                 cmdp: cast(ubyte*)&getConfigurationCDB,
                 sbp: null,
-                timeout: 5000
-            };
+                timeout: 5000};
 
-            int status;
-            return ioctlWrapper(drivePathName, SG_IO, status, &header);
-        }
+                int status;
+                return ioctlWrapper(drivePathName, SG_IO, status, &header);
+            }
 
-        auto openImpl(string drivePathName)
-        {
-            return ioctlWrapper(drivePathName, CDROMEJECT).ok;
-        }
+            auto openImpl(string drivePathName)
+            {
+                return ioctlWrapper(drivePathName, CDROMEJECT).ok;
+            }
 
-        auto closeImpl(string drivePathName)
-        {
-            return ioctlWrapper(drivePathName, CDROMCLOSETRAY).ok;
+            auto closeImpl(string drivePathName)
+            {
+                return ioctlWrapper(drivePathName, CDROMCLOSETRAY).ok;
+            }
         }
     }
-}
